e6cc09b6c2fa54bca1901522e93136dcbaf2c1e6
Raul Oviedo Torres <raul04x@outlook.com>
2022-10-10 20:51:35 -0500

    Adding better lookandfeel

--- garage-sale.js
+++ garage-sale.js
@@ -6,6 +6,7 @@ function drawProducts(index, product) {
   let buttonsImg = "";
   let images = "";
   const target = `ci-${index}`;
+  const vendida = '<span class="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-success">Vendida</span>';
   
   product.photos.forEach((p, i) => {
     buttonsImg += `<button type="button" data-bs-target="#${target}" data-bs-slide-to="${i}" class="active" aria-current="true" aria-label="Foto ${i+1}"></button>`;
@@ -13,33 +14,34 @@ function drawProducts(index, product) {
   });
 
   const cardProduct = `
-    <div class="card mt-3">
+  <div class="col">
+    <div class="card h-100 ${product.isSelled ? 'border-success' : ''}">
         <h5 class="card-header">
-            <div id="${target}" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    ${buttonsImg}
-                </div>
-                <div class="carousel-inner">
-                    ${images}
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#${target}"
-                    data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#${target}"
-                    data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+          <div id="${target}" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              ${buttonsImg}
             </div>
+            <div class="carousel-inner">
+              ${images}
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#${target}" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#${target}" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+          ${product.isSelled ? vendida : ''}
         </h5>
         <div class="card-body">
-            <h5 class="card-title">${product.name}</h5>
-            <p class="card-text">${product.description}</p>
-            <p class="card-text"><span class="fw-bold">Precio:</span> ${product.price}</p>
+          <h5 class="card-title">${product.name}</h5>
+          <p class="card-text">${product.description}</p>
+          <p class="card-text ${product.isSelled ? 'text-decoration-line-through' : ''}"><span class="fw-bold">Precio:</span> ${product.price}</p>
         </div>
-    </div>`;
+    </div>
+  </div>`;
 
   $("#root").append(cardProduct);
 }
