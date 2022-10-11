b1f5124dae48157100e18c133ce23f1a9add5945
Raul Oviedo Torres <raul04x@outlook.com>
2022-10-10 22:38:38 -0500

    Agegando carrito de compras con funciones basicas

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Simple Garage Sale
 
 This is a pretty simple garage sale page, using a JSON file to fill the data for each item and saving the images in `\img\` folder
+
+## Copyright
+Copyright © 2022
+
+## License
+garage-sale is under MIT license - http://www.opensource.org/licenses/mit-license.php
--- garage-sale.js
+++ garage-sale.js
@@ -1,21 +1,34 @@
-$.getJSON("info/items.json", data => {
+var myItems = [];
+var items = [];
+toastr.options.preventDuplicates = true;
+
+$.getJSON("info/items.json", (data) => {
   data.products.forEach((p, i) => drawProducts(i, p));
+  items = data.products;
 });
 
 function drawProducts(index, product) {
   let buttonsImg = "";
   let images = "";
   const target = `ci-${index}`;
-  const vendida = '<span class="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-success">Vendida</span>';
-  
+  const vendida =
+    '<span class="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-success">Vendida</span>';
+  product.id = index;
+
   product.photos.forEach((p, i) => {
-    buttonsImg += `<button type="button" data-bs-target="#${target}" data-bs-slide-to="${i}" class="active" aria-current="true" aria-label="Foto ${i+1}"></button>`;
-    images += `<div class="carousel-item ${i === 0 ? 'active' : ''}"><img src="${p.path}" class="d-block ${p.style}" alt="${product.name}"></div>`;
+    buttonsImg += `<button type="button" data-bs-target="#${target}" data-bs-slide-to="${i}" class="active" aria-current="true" aria-label="Foto ${
+      i + 1
+    }"></button>`;
+    images += `<div class="carousel-item ${
+      i === 0 ? "active" : ""
+    }"><img src="${p.path}" class="d-block ${p.style}" alt="${
+      product.name
+    }"></div>`;
   });
 
   const cardProduct = `
   <div class="col">
-    <div class="card h-100 ${product.isSelled ? 'border-success' : ''}">
+    <div class="card h-100 ${product.isSelled ? "border-success" : ""}">
         <h5 class="card-header">
           <div id="${target}" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
@@ -33,15 +46,58 @@ function drawProducts(index, product) {
               <span class="visually-hidden">Next</span>
             </button>
           </div>
-          ${product.isSelled ? vendida : ''}
+          ${product.isSelled ? vendida : ""}
         </h5>
         <div class="card-body">
           <h5 class="card-title">${product.name}</h5>
           <p class="card-text">${product.description}</p>
-          <p class="card-text ${product.isSelled ? 'text-decoration-line-through' : ''}"><span class="fw-bold">Precio:</span> ${product.price}</p>
+          <p class="card-text ${product.isSelled ? "text-decoration-line-through" : ""}"><span class="fw-bold">Precio:</span> ${product.price}</p>
+          ${
+            product.isSelled
+              ? ""
+              : `<button onclick="addItem(${product.id})" class="btn btn-success"><i class="fa-solid fa-cart-plus"></i></button>`
+          }
         </div>
     </div>
   </div>`;
 
   $("#root").append(cardProduct);
+}
+
+function addItem(id) {
+  let myItem = myItems.find((i) => i.id === id);
+  let item = items.find((i) => i.id === id);
+
+  if (!myItem && item) {
+    myItems.push(item);
+    setNumberItems();
+    toastr.info(`El artículo <strong>${myItem.name}</strong> se ha agregado al carrito!`);
+  } else {
+    toastr.warning(`El artículo <strong>${myItem.name}</strong> ya se encuentra en el carrito!`);
+  }
+}
+
+function removeItem(id) {
+  myItems = myItems.filter((i) => i.id !== id);
+  setNumberItems();
+}
+
+function setNumberItems() {
+  if (myItems.length > 0) {
+    $("#spItems").removeClass("invisible");
+    $("#spItems").text(myItems.length);
+  } else {
+    $("#spItems").addClass("invisible");
+  }
+}
+
+function displayMyItems() {
+  let items = myItems.length > 0 ? "A continuación se listan sus productos:" : "";
+  myItems.forEach((p, i) => {
+    items += `<p><strong>${i + 1})</strong> ${p.name} (${p.price})</p>`;
+  });
+
+  if (items) {
+    toastr.success(items);
+  }
 }
